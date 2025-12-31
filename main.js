118b6775ad1b38da13248490d024f9955531596a
NookieAI <188130079+NookieAI@users.noreply.github.com>
2025-12-31 17:13:42 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    maximized: true,
+    icon: path.join(__dirname, 'assets', 'icon1.ico'), // Set the app icon
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
